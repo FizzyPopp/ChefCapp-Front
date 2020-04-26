e9ca787d4f7383efd3a6465dba9d4a656e0f1ea0
markassa <markassa@gmail.com>
2020-04-26 19:10:13 -0400

    Adding temporary auth and db files from tutorial

---
+++ chef_capp/lib/services/database.dart
@@ -0,0 +1,64 @@
+//import 'package:brew_crew/models/brew.dart';
+//import 'package:brew_crew/models/user.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+// https://github.com/iamshaunjp/flutter-firebase
+// https://www.youtube.com/watch?v=_SHssHJJhAI&list=PL4cUxeGkcC9j--TKIdkb3ISfRbJeJYQwC&index=10
+
+// THIS AND auth.dart ARE FROM THE TUTORIAL ABOVE
+// NEED TO CONVERT PROVIDER TYPE THINGS, CAN GET RID OF MOST STREAMS
+
+class DatabaseService {
+
+  final String uid;
+  DatabaseService({ this.uid });
+
+  // collection reference
+  final CollectionReference brewCollection = Firestore.instance.collection('brews');
+
+  Future<void> updateUserData(String sugars, String name, int strength) async {
+    return await brewCollection.document(uid).setData({
+      'sugars': sugars,
+      'name': name,
+      'strength': strength,
+    });
+  }
+
+  /*
+  // get brews stream
+  Stream<List<Brew>> get brews {
+    return brewCollection.snapshots()
+        .map(_brewListFromSnapshot);
+  }
+
+  // brew list from snapshot
+  List<Brew> _brewListFromSnapshot(QuerySnapshot snapshot) {
+    return snapshot.documents.map((doc){
+      //print(doc.data);
+      return Brew(
+          name: doc.data['name'] ?? '',
+          strength: doc.data['strength'] ?? 0,
+          sugars: doc.data['sugars'] ?? '0'
+      );
+    }).toList();
+  }
+   */
+
+  /*
+  // user data from snapshots
+  UserData _userDataFromSnapshot(DocumentSnapshot snapshot) {
+    return UserData(
+        uid: uid,
+        name: snapshot.data['name'],
+        sugars: snapshot.data['sugars'],
+        strength: snapshot.data['strength']
+    );
+  }
+
+  // get user doc stream
+  Stream<UserData> get userData {
+    return brewCollection.document(uid).snapshots()
+        .map(_userDataFromSnapshot);
+  }
+   */
+}
