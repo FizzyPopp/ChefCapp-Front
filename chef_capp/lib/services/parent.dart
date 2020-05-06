d84d87323032f41d9f6cc548462058af2e934b53
James Liu <liujames.q@gmail.com>
2020-05-05 22:47:45 -0400

    WIP

--- chef_capp/lib/services/parent.dart
+++ chef_capp/lib/services/parent.dart
@@ -1,4 +1,5 @@
 import 'package:chef_capp/index.dart';
+import 'package:sqflite/sqflite.dart';
 
 class ParentController with ChangeNotifier {
   static const int SEED = 2;
@@ -8,5 +9,12 @@ class ParentController with ChangeNotifier {
   - dispatches all other controllers (ensures there's only ever one instance of the Discover controller, etc.)
   - most screens will need access to this controller, as they will ask it for other controllers
   - single universal source of truth
-   */
+  */
+  static User user;
+  static _dbLocal = await openDatabase('cc_local.db');
+//  static _dbChefCapp = await openDatabase('assets/chef_capp.db');
+  init() {
+
+
+  }
 }
