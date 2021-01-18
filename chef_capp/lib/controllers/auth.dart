4b17a66185d71d4cf1a1065ebb8d33ec08cd6583
FizzyPopp <ardakizilcay@hotmail.com>
2021-01-17 20:48:11 -0500

    Password show & hide added to signup and login

--- chef_capp/lib/controllers/auth.dart
+++ chef_capp/lib/controllers/auth.dart
@@ -20,6 +20,9 @@ class AuthController with ChangeNotifier {
    * Super users obv have access to everything.
    */
 
+  void notify() {
+    notifyListeners();
+  }
 
   Future<void> handleWelcomeBrowse(BuildContext context) {
     ParentService.auth.loginAnon().then((success) async {
--- chef_capp/lib/screens/authentication/signup.dart
+++ chef_capp/lib/screens/authentication/signup.dart
@@ -1,108 +1,28 @@
 import 'package:chef_capp/index.dart';
+import 'package:provider/provider.dart';
 
 class SignUp extends StatelessWidget {
+  bool _obscurePassword = true;
+
+  Icon _obscurePasswordIcon = Icon(Icons.remove_red_eye_outlined);
+
+  void _toggleObscurePassword() {
+    _obscurePassword = !_obscurePassword;
+
+    if (_obscurePassword) {
+      _obscurePasswordIcon = Icon(Icons.remove_red_eye_outlined);
+    } else if (!_obscurePassword) {
+      _obscurePasswordIcon = Icon(Icons.remove_red_eye);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: ListView(
-          children: [
-            Container(
-              alignment: Alignment.centerLeft,
-              height: 48.0,
-              child: FlatButton.icon(
-                icon: Icon(
-                  Icons.arrow_back_ios_sharp,
-                  size: 14.0,
-                ),
-                label: Text(
-                  'Back',
-                  //style: CCText.lightButton,
-                ),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 32.0,
-                vertical: 12.0,
-              ),
-              child: AutoSizeText(
-                "Sign up to start cooking",
-                style: CCText.mobileDisplayLarge,
-                maxLines: 2,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 32.0,
-                vertical: 16.0,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Name',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                  SizedBox(height: 16.0,),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                  SizedBox(height: 16.0,),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                      border: OutlineInputBorder(),
-                      suffixIcon: Icon(Icons.remove_red_eye),
-                    ),
-                    obscureText: true,
-                  ),
-                  SizedBox(height: 16.0,),
-                  Text(
-                    'Password must be a minimum of 8 characters long,'
-                        ' including one number (0-9), one special character'
-                        ' (~!@#\$%^&), one uppercase letter, and one lowercase '
-                        'letter.',
-                    style: TextStyle(
-                      color: CCColors.secondary,
-                    ),
-                  ),
-                  SizedBox(height: 16.0,),
-                  Text(
-                      'By clicking Sign Up, you agree and consent to the User'
-                      ' Agreement and the Privacy Policy.'
-                  ),
-                  SizedBox(height: 16.0,),
-                  GradientButton(
-                    child: Text(
-                      "SIGN UP",
-                      style: CCText.darkButton,
-                    ),
-                    gradient: CCColors.primaryGradient,
-                    onPressed: () {
-
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+    return ChangeNotifierProvider.value(
+      value: ParentController.auth,
+      child: Scaffold(
+        body: SafeArea(
+          child: ListView(
             children: [
               Container(
                 alignment: Alignment.centerLeft,
@@ -111,7 +31,6 @@ class SignUp extends StatelessWidget {
                   icon: Icon(
                     Icons.arrow_back_ios_sharp,
                     size: 14.0,
-                    color: CCText.lightButton.color,
                   ),
                   label: Text(
                     'Back',
@@ -122,80 +41,82 @@ class SignUp extends StatelessWidget {
                   },
                 ),
               ),
-              Expanded(
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 32.0,
+                  vertical: 12.0,
+                ),
+                child: AutoSizeText(
+                  "Sign up to start cooking",
+                  style: CCText.mobileDisplayLarge,
+                  maxLines: 2,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 32.0,
+                  vertical: 16.0,
+                ),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.stretch,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 32.0,
-                        vertical: 12.0,
-                      ),
-                      child: AutoSizeText(
-                        "Sign up to start cooking",
-                        style: CCText.mobileDisplayLarge,
-                        maxLines: 2,
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Name',
+                        border: OutlineInputBorder(),
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 32.0,
-                        vertical: 16.0,
+                    SizedBox(height: 16.0,),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        border: OutlineInputBorder(),
                       ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          TextFormField(
-                            decoration: InputDecoration(
-                              labelText: 'Name',
-                              border: OutlineInputBorder(),
-                            ),
-                          ),
-                          SizedBox(height: 16.0,),
-                          TextFormField(
-                            decoration: InputDecoration(
-                              labelText: 'Email',
-                              border: OutlineInputBorder(),
-                            ),
-                          ),
-                          SizedBox(height: 16.0,),
-                          TextFormField(
-                            decoration: InputDecoration(
-                              labelText: 'Password',
-                              border: OutlineInputBorder(),
-                              suffixIcon: Icon(Icons.remove_red_eye),
-                            ),
-                            obscureText: true,
-                          ),
-                          Container(
-                            alignment: Alignment.centerLeft,
-                            child: FlatButton(
-                              padding: EdgeInsets.symmetric(horizontal: 0),
-                              child: Text(
-                                'Forgot Password?',
-                                style: TextStyle(
-                                  color: Color(0xFF4E4B66),
-                                  letterSpacing: 0.20,
-                                ),
-                              ),
+                    ),
+                    SizedBox(height: 16.0,),
+                    Consumer<AuthController>(
+                      builder: (context, data, _) {
+                        return TextFormField(
+                          decoration: InputDecoration(
+                            labelText: 'Password',
+                            border: OutlineInputBorder(),
+                            suffixIcon: IconButton(
+                              icon: _obscurePasswordIcon,
                               onPressed: () {
-
+                                _toggleObscurePassword();
+                                data.notify();
                               },
                             ),
                           ),
-                          GradientButton(
-                            child: Text(
-                              "LOG IN",
-                              style: CCText.darkButton,
-                            ),
-                            onPressed: () {
-
-                            },
-                            gradient: CCColors.secondaryGradient,
-                          ),
-                        ],
+                          obscureText: _obscurePassword,
+                        );
+                      }
+                    ),
+                    SizedBox(height: 16.0,),
+                    Text(
+                      'Password must be a minimum of 8 characters long,'
+                          ' including one number (0-9), one special character'
+                          ' (~!@#\$%^&), one uppercase letter, and one lowercase '
+                          'letter.',
+                      style: TextStyle(
+                        color: CCColors.secondary,
                       ),
+                    ),
+                    SizedBox(height: 16.0,),
+                    Text(
+                        'By clicking Sign Up, you agree and consent to the User'
+                        ' Agreement and the Privacy Policy.'
+                    ),
+                    SizedBox(height: 16.0,),
+                    GradientButton(
+                      child: Text(
+                        "SIGN UP",
+                        style: CCText.darkButton,
+                      ),
+                      gradient: CCColors.primaryGradient,
+                      onPressed: () {
+
+                      },
                     ),
                   ],
                 ),
