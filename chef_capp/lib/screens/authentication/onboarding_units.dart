d1e38d57982d8ffcf42043fe016a8cfa095ba922
Liam Marcassa <markassa@gmail.com>
2021-03-04 20:49:44 -0500

    Integrate preferences controller with screen

--- chef_capp/lib/screens/authentication/onboarding_units.dart
+++ chef_capp/lib/screens/authentication/onboarding_units.dart
@@ -1,174 +1,168 @@
 import 'package:chef_capp/index.dart';
 import 'package:chef_capp/screens/authentication/onboarding_allergies.dart';
+import 'package:provider/provider.dart';
 
-class OnboardingUnits extends StatefulWidget {
-  @override
-  _OnboardingUnitsState createState() => _OnboardingUnitsState();
-}
-
-class _OnboardingUnitsState extends State<OnboardingUnits> {
-  bool _metricVolume = true;
-  bool _metricMass = true;
-  bool _metricTemp = true;
-
-  double _chipWidth = 100.0;
+class OnboardingUnits extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: ListView(
-          children: [
-            Container(
-              height: 48.0,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 32.0,
-                    vertical: 12.0,
+    return ChangeNotifierProvider.value(
+      value: ParentController.preferences,
+      child: Scaffold(
+        body: SafeArea(
+          child: ListView(
+            children: [
+              Container(
+                height: 48.0,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 32.0,
+                      vertical: 12.0,
+                    ),
+                    child: AutoSizeText(
+                      "What measurement units do you use?",
+                      style: CCText.mobileDisplayLarge,
+                      maxLines: 3,
+                    ),
                   ),
-                  child: AutoSizeText(
-                    "What measurement units do you use?",
-                    style: CCText.mobileDisplayLarge,
-                    maxLines: 3,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 32.0,
-                    vertical: 12.0,
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        'VOLUME',
-                        style: CCText.tempSubHeader,
-                      ),
-                      SizedBox(height: 10.0,),
-                      Wrap(
-                        children: <Widget>[
-                          UnitChip(
-                            firstLabel: 'milliliter, liter',
-                            secondLabel: '(mL,L)',
-                            selected: _metricVolume,
-                            onSelected:  (bool selected) {
-                              setState(() {
-                                _metricVolume = true;
-                              });
-                            },
-                          ),
-                          SizedBox(width: 8.0,),
-                          UnitChip(
-                            firstLabel: 'ounce, gallon',
-                            secondLabel: '(oz, gal.)',
-                            selected: !_metricVolume,
-                            onSelected:  (bool selected) {
-                              setState(() {
-                                _metricVolume = false;
-                              });
-                            },
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 10.0,),
-                      Text(
-                        'WEIGHT',
-                        style: CCText.tempSubHeader,
-                      ),
-                      SizedBox(height: 10.0,),
-                      Wrap(
-                        children: <Widget>[
-                          UnitChip(
-                            firstLabel: 'gram, kilogram',
-                            secondLabel: '(g, kg)',
-                            selected: _metricMass,
-                            onSelected:  (bool selected) {
-                              setState(() {
-                                _metricMass = true;
-                              });
-                            },
-                          ),
-                          SizedBox(width: 8.0,),
-                          UnitChip(
-                            firstLabel: 'ounce, pound',
-                            secondLabel: '(oz, lb.)',
-                            selected: !_metricMass,
-                            onSelected:  (bool selected) {
-                              setState(() {
-                                _metricMass = false;
-                              });
-                            },
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 10.0,),
-                      Text(
-                        'TEMPERATURE',
-                        style: CCText.tempSubHeader,
-                      ),
-                      SizedBox(height: 10.0,),
-                      Wrap(
-                        children: <Widget>[
-                          UnitChip(
-                            firstLabel: 'Celcius',
-                            selected: _metricTemp,
-                            onSelected:  (bool selected) {
-                              setState(() {
-                                _metricTemp = true;
-                              });
-                            },
-                          ),
-                          SizedBox(width: 8.0,),
-                          UnitChip(
-                            firstLabel: 'Fahrenheit',
-                            selected: !_metricTemp,
-                            onSelected:  (bool selected) {
-                              setState(() {
-                                _metricTemp = false;
-                              });
-                            },
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 8.0,),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 32.0,
-                    vertical: 16.0,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      GradientButton(
-                        child: Text(
-                          "NEXT",
-                          style: CCText.darkButton,
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 32.0,
+                      vertical: 12.0,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          'VOLUME',
+                          style: CCText.tempSubHeader,
                         ),
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => OnboardingAllergies()
-                            ),
-                          );
-                        },
-                        gradient: CCColors.primaryGradient,
-                      )
-                    ],
+                        SizedBox(height: 10.0,),
+                        Consumer<PreferencesController>(
+                          builder: (context, data, _) {
+                            return Wrap(
+                              children: <Widget>[
+                                UnitChip(
+                                  firstLabel: 'milliliter, liter',
+                                  secondLabel: '(mL,L)',
+                                  selected: data.metricVolume,
+                                  onSelected:  (bool selected) {
+                                    data.metricVolume = true;
+                                  },
+                                ),
+                                SizedBox(width: 8.0,),
+                                UnitChip(
+                                  firstLabel: 'ounce, gallon',
+                                  secondLabel: '(oz, gal.)',
+                                  selected: !data.metricVolume,
+                                  onSelected:  (bool selected) {
+                                    data.metricVolume = false;
+                                  },
+                                ),
+                              ],
+                            );
+                          }
+                        ),
+                        SizedBox(height: 10.0,),
+                        Text(
+                          'WEIGHT',
+                          style: CCText.tempSubHeader,
+                        ),
+                        SizedBox(height: 10.0,),
+                        Consumer<PreferencesController>(
+                          builder: (context, data, _) {
+                            return Wrap(
+                              children: <Widget>[
+                                UnitChip(
+                                  firstLabel: 'gram, kilogram',
+                                  secondLabel: '(g, kg)',
+                                  selected: data.metricWeight,
+                                  onSelected:  (bool selected) {
+                                    data.metricWeight = true;
+                                  },
+                                ),
+                                SizedBox(width: 8.0,),
+                                UnitChip(
+                                  firstLabel: 'ounce, pound',
+                                  secondLabel: '(oz, lb.)',
+                                  selected: !data.metricWeight,
+                                  onSelected:  (bool selected) {
+                                    data.metricWeight = false;
+                                  },
+                                ),
+                              ],
+                            );
+                          }
+                        ),
+                        SizedBox(height: 10.0,),
+                        Text(
+                          'TEMPERATURE',
+                          style: CCText.tempSubHeader,
+                        ),
+                        SizedBox(height: 10.0,),
+                        Consumer<PreferencesController>(
+                          builder: (context, data, _) {
+                            return Wrap(
+                              children: <Widget>[
+                                UnitChip(
+                                  firstLabel: 'Celcius',
+                                  selected: data.metricTemperature,
+                                  onSelected:  (bool selected) {
+                                    data.metricTemperature = true;
+                                  },
+                                ),
+                                SizedBox(width: 8.0,),
+                                UnitChip(
+                                  firstLabel: 'Fahrenheit',
+                                  selected: !data.metricTemperature,
+                                  onSelected:  (bool selected) {
+                                    data.metricTemperature = false;
+                                  },
+                                ),
+                              ],
+                            );
+                          }
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                  SizedBox(height: 8.0,),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 32.0,
+                      vertical: 16.0,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        GradientButton(
+                          child: Text(
+                            "NEXT",
+                            style: CCText.darkButton,
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => OnboardingAllergies()
+                              ),
+                            );
+                          },
+                          gradient: CCColors.primaryGradient,
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
